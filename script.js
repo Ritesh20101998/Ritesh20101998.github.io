5aa6146fbe63cf472cc5f33a901e2dd3bdbaba52
Ritesh <kothawaderitesh2010@gmail.com>
2023-05-04 23:24:25 +0530

    portfolio change

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
-const header = document.querySelector("header");
-window.addEventListener("scroll", function() {
-    header.classList.toggle("sticky", window.scrollY > 100)
-})
+
+
+let menu=document.getElementById("menu-icon");
+let navbar=document.querySelector(".navbar");
+let m=document.querySelector(".main-body")
+let count=0;
+menu.addEventListener('click',()=>{
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+    
+    m.style.marginTop="200px";
+    count++;
+    if(count%2==0){
+        m.style.marginTop="0px";
+
+    }
+
+    if(m.classList.contains("m-t")){
+        m.classList.remove("m-t");
+    }
+    else{
+        m.classList.add("m-t");
+    }
+});
+
+
+GitHubCalendar(".calendar", "KrunalGurao");
+GitHubCalendar(".calendar", "KrunalGurao", { responsive: true });
+
+
+function resume(){
+    window.open("https://drive.google.com/file/d/1wZ0iTA-zl_4-bn_NZv5dK4cMWTl9bONX/view?usp=share_link")
+}
