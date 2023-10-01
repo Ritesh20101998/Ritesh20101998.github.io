a1e10d92b9c203423d72db703a750279587c6cae
Ritesh <kothawaderitesh2010@gmail.com>
2023-10-01 23:41:39 +0530

    tools added

--- script.js
+++ script.js
@@ -1,56 +1,41 @@
- 
-let menu=document.getElementById("menu-icon");
-let navbar=document.querySelector(".navbar");
-let m=document.querySelector(".main-body")
-let count=0;
-menu.addEventListener('click',()=>{
-    menu.classList.toggle('bx-x');
-    navbar.classList.toggle('open');
-    
-    m.style.marginTop="200px";
-    count++;
-    if(count%2==0){
-        m.style.marginTop="0px";
+// JavaScript function to toggle the navigation menu
+function toggleMenu() {
+  var navMenu = document.getElementById('nav-menu'); // Get the navigation menu element
 
-    }
+  // Toggle the visibility of the navigation menu
+  if (navMenu.style.display === 'block') {
+    navMenu.style.display = 'none';
+  } else {
+    navMenu.style.display = 'block';
+  }
+}
 
-    if(m.classList.contains("m-t")){
-        m.classList.remove("m-t");
-    }
-    else{
-        m.classList.add("m-t");
-    }
+// JavaScript function to scroll to the top of the page
+function scrollToTop() {
+  var body = document.body; // Get the body element
+  body.scrollTop = 0; // Scroll to the top of the page
+}
+
+// Add a click event listener to the menu icon
+var menuIcon = document.getElementById('menu-icon');
+menuIcon.addEventListener('click', toggleMenu);
+
+// Add click event listeners to navigation links
+var navLinks = document.querySelectorAll('#nav-menu a');
+navLinks.forEach(function(link) {
+  link.addEventListener('click', function() {
+    toggleMenu(); // Close the navigation menu
+    scrollToTop(); // Scroll to the top of the page
+  });
 });
 
-function myFunction() {
-    var x = document.getElementById("nav-menu");
-    if (x.className === "navbar") {
-      x.className += " responsive";
-    } else {
-      x.className = "navbar";
-    }
-}
 
 GitHubCalendar(".calendar", "Ritesh20101998");
 GitHubCalendar(".calendar", "Ritesh20101998", { responsive: true });
 
-
 function resume(){
     window.open("https://drive.google.com/file/d/1aB0sJXQom6RrLbDu7iAs1ljX-JkDS5iQ/view?usp=sharing")
 }
-
-const menus = document.querySelectorAll("#nav-menu .nav-link");
-
-menus.forEach((menu) => {
-    menu.addEventListener("click", (e) => {
-      // Remove selected class from all size options
-      menus.forEach((menu) => {
-        // menu.classList.remove("active_menu");
-      });
-      // Add active_menu class to the clicked size option
-      menu.classList.add("active_menu");
-    });
-});
 
 function sendEmail(){
     Email.send({
